b84bc78e24e8763d8ba8b0f53cb5bf37d4b7aae7
joshmagia <58450898+joshmagia@users.noreply.github.com>
2020-01-30 11:37:35 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -376,7 +376,8 @@ function resize(image, wantedHeight, wantedWidth) {
 
     // Draw the image to the same dimensions wanted (also same as the canvas)
     // drawImage(img,x,y,width,height);
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    // ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 50, 50, canvas.width, canvas.height);
 
     // Clear the dimensions div incase of consecutive uploads
     $("#resized-xy").html("");
